06b4cac033ef5db015249ab98a1e8535d06f146f
finnleygrange <per.finnleygrange@icloud.com>
2025-05-18 23:19:28 +0100

    Create Games page to display owned games separately from Index

--- Pages/Games.cshtml.cs
+++ Pages/Games.cshtml.cs
@@ -5,27 +5,27 @@ using System.Security.Claims;
 
 namespace SteamShelf.Pages
 {
-    public class IndexModel : PageModel
+    public class GamesModel : PageModel
     {
         public SteamPlayer? SteamPlayer { get; set; }
         public List<SteamGame>? SteamOwnedGames { get; set; }
 
         private readonly SteamApiService _steamService;
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<GamesModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger, SteamApiService steamService)
+        public GamesModel(ILogger<GamesModel> logger, SteamApiService steamService)
         {
             _logger = logger;
             _steamService = steamService;
         }
 
-        public async Task OnGetAsync()
+        public async Task OnGet()
         {
             var openId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
             if (!string.IsNullOrEmpty(openId))
             {
-                SteamPlayer = await _steamService.GetPlayerAsync(openId);
+                SteamOwnedGames = await _steamService.GetOwnedGamesAsync(openId);
             }
         }
     }
