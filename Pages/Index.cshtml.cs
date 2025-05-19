59db4842cd941204dccb5435a5598c37e4079850
finnleygrange <per.finnleygrange@icloud.com>
2025-05-19 02:49:20 +0100

    Add Steam user stats and recently played games to index page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -10,6 +10,13 @@ namespace SteamShelf.Pages
     {
         public SteamPlayer? SteamPlayer { get; set; }
         public List<SteamGame>? SteamOwnedGames { get; set; }
+        public int TotalGames => SteamOwnedGames?.Count ?? 0;
+        public int TotalplaytimeHours => (int)((SteamOwnedGames?.Sum(g => g.PlaytimeForever) ?? 0) / 60.0);
+        public double PlaytimeLast2WeeksHours => (SteamOwnedGames?.Sum(g => g.Playtime2Weeks) ?? 0) / 60.0;
+        public List<SteamGame> RecentlyPlayedGames => SteamOwnedGames?.Where(g => g.Playtime2Weeks > 0)
+                    .OrderByDescending(g => g.Playtime2Weeks)
+                    .ToList() ?? new List<SteamGame>();
+
 
         private readonly SteamApiService _steamService;
         private readonly ILogger<IndexModel> _logger;
