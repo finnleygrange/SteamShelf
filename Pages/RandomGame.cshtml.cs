69f198d292e01c9f60f71315fa130e41707c461f
finnleygrange <per.finnleygrange@icloud.com>
2025-05-20 21:08:26 +0100

    Add only unplayed games filter logic

--- Pages/RandomGame.cshtml.cs
+++ Pages/RandomGame.cshtml.cs
@@ -11,6 +11,10 @@ namespace SteamShelf.Pages
     {
         public SteamPlayer? SteamPlayer { get; set; }
         public List<SteamGame>? SteamOwnedGames { get; set; }
+        public SteamGame? RandomGame {  get; set; }
+
+        [BindProperty]
+        public bool OnlyUnplayedGames { get; set; } = false;
 
         private readonly SteamApiService _steamService;
         private readonly ILogger<RandomGameModel> _logger;
@@ -33,9 +37,44 @@ namespace SteamShelf.Pages
                 ViewData["SteamPlayer"] = SteamPlayer;
 
                 Random rnd = new Random();
-                var unplayedGames = SteamOwnedGames?.ToList();
-                var randomGame = unplayedGames?[rnd.Next(unplayedGames.Count)];
+
+                if (!OnlyUnplayedGames)
+                {
+                    RandomGame = SteamOwnedGames?[rnd.Next(SteamOwnedGames.Count)];
+                }
+                else
+                {
+                    var unplayedGames = SteamOwnedGames?.Where(g => g.PlaytimeForever == 0).ToList();
+                    RandomGame = unplayedGames?[rnd.Next(unplayedGames.Count)];
+                }
             }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var openId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (!string.IsNullOrEmpty(openId))
+            {
+                SteamPlayer = await _steamService.GetPlayerAsync(openId);
+                SteamOwnedGames = await _steamService.GetOwnedGamesAsync(openId);
+
+                var rnd = new Random();
+
+                if (OnlyUnplayedGames)
+                {
+                    var unplayed = SteamOwnedGames?.Where(g => g.PlaytimeForever == 0).ToList();
+                    if (unplayed != null && unplayed.Count > 0)
+                        RandomGame = unplayed[rnd.Next(unplayed.Count)];
+                }
+                else
+                {
+                    if (SteamOwnedGames != null && SteamOwnedGames.Count > 0)
+                        RandomGame = SteamOwnedGames[rnd.Next(SteamOwnedGames.Count)];
+                }
+            }
+
+            return Page();
         }
     }
 }
