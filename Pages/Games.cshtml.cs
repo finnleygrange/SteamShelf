d8d5a04ccc8fd39063a0072560e091bbe720f6ca
finnleygrange <per.finnleygrange@icloud.com>
2025-05-20 22:28:38 +0100

    Add authorization to Games and RandomGame pages to restrict access

--- Pages/Games.cshtml.cs
+++ Pages/Games.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SteamShelf.Model;
 using SteamShelf.Services;
@@ -5,6 +6,7 @@ using System.Security.Claims;
 
 namespace SteamShelf.Pages
 {
+    [Authorize]
     public class GamesModel : PageModel
     {
         public string ViewType { get; set; } = "grid";
--- Pages/RandomGame.cshtml.cs
+++ Pages/RandomGame.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -7,6 +8,7 @@ using System.Security.Claims;
 
 namespace SteamShelf.Pages
 {
+    [Authorize]
     public class RandomGameModel : PageModel
     {
         public SteamPlayer? SteamPlayer { get; set; }
